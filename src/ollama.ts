1a673f0185085d4bf6489c32dee0c079a5304479
exsjabe <jakob.bergstrom@explizit.se>
2025-04-11 23:31:23 +0200

    langgraph qwern testing

--- src/ollama.ts
+++ src/ollama.ts
@@ -1,43 +1,75 @@
 import {ChatOllama} from '@langchain/ollama';
 import {Calculator} from '@langchain/community/tools/calculator';
-import {DynamicTool} from '@langchain/core/tools';
+import * as agents from '@langchain/langgraph/prebuilt';
+import {DynamicStructuredTool} from '@langchain/core/tools';
 import readline from 'node:readline';
-import {AgentExecutor, createToolCallingAgent} from 'langchain/agents';
-import {ChatPromptTemplate, MessagesPlaceholder} from '@langchain/core/prompts';
-import {getSystemPrompt} from './system.ts';
+import {execAsync, getSystemPrompt} from './system.ts';
+import {z} from 'zod';
+import {pwshCommand} from './wmi.ts';
+import {MemorySaver, type Messages} from '@langchain/langgraph';
+import prexit from 'prexit';
+import {AIMessage} from '@langchain/core/messages';
+import util from 'node:util';
 
 export async function Ollama() {
+  const modelName = 'qwen2.5:14b';
   // Set up the local model through Ollama
   const model = new ChatOllama({
     baseUrl: 'http://localhost:11434',
-    model: 'mistral',
-    temperature: 0.2,
+    model: modelName,
+    temperature: 1,
   });
+  prexit(() => execAsync(`ollama stop ${modelName}`, {}).catch(console.warn));
 
   // Define tools
-  const tools = [new Calculator()];
+  const tools = [
+    new Calculator(),
+    new DynamicStructuredTool({
+      name: 'read-process-env',
+      description:
+        'Returns an object containing all environment variables for the running process. Use this to read the system environment. You can optionally provide a key to a read a specific variable',
+      schema: z.object({
+        key: z
+          .string()
+          .optional()
+          .describe('An optional key to read a specific variable.'),
+      }),
+      func: async (args) => {
+        if (args.key) {
+          return process.env[args.key] ?? '';
+        }
+
+        return JSON.stringify(process.env);
+      },
+    }),
+    new DynamicStructuredTool({
+      name: 'read-only-powershell-command',
+      description:
+        'Run a command on the users computer in powershell with -LanguageMode ConstrainedLanguage. Use this to answer questions about the users system.',
+      schema: z.object({
+        command: z.string().describe('The command to run'),
+      }),
+      func: async (args) => {
+        try {
+          const result = await pwshCommand(args.command);
+          return result;
+        } catch (e) {
+          console.error(e);
+          throw e;
+        }
+      },
+    }),
+  ];
 
   const systemPrompt = await getSystemPrompt();
 
-  // Create the agent prompt
-  const prompt = ChatPromptTemplate.fromMessages([
-    ['system', systemPrompt],
-    ['human', '{input}'],
-    new MessagesPlaceholder('agent_scratchpad'),
-  ]);
-
   // Create the agent
-  const agent = createToolCallingAgent({
+  const memory = new MemorySaver();
+  const agent = agents.createReactAgent({
     llm: model,
     tools,
-    prompt,
-  });
-
-  // Create the executor
-  const agentExecutor = new AgentExecutor({
-    agent,
-    tools,
-    verbose: false,
+    prompt: systemPrompt,
+    checkpointSaver: memory,
   });
 
   const rl = readline.createInterface({
@@ -65,9 +97,31 @@ export async function Ollama() {
       continue;
     }
 
+    const messages: Messages = [{role: 'human', content: line}];
+    const config = {configurable: {thread_id: '1'}};
     console.log('Contacting AI...');
-    const res = await agentExecutor.invoke({input: line});
-    console.log(res);
+    const stream = await agent.stream(
+      {messages},
+      {...config, streamMode: 'values'},
+    );
+
+    for await (const {messages} of stream) {
+      let msg = messages.at(-1);
+
+      if (!(msg instanceof AIMessage)) continue;
+      if (msg?.content) {
+        console.log(msg.content);
+      } else if (msg.tool_calls?.length) {
+        console.log(
+          util.inspect(
+            msg.tool_calls.map((tc) => ({[tc.name]: tc.args})),
+            {depth: 5},
+          ),
+        );
+      }
+
+      console.log('-----\n');
+    }
     process.stdout.write('\n> ');
   }
 }
